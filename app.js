2051f883a548eaa716a0bd1ad57da4733331862d
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2022-10-20 13:52:10 -0500

    Mejorando el proyecto

--- app.js
+++ app.js
@@ -9,23 +9,45 @@ const loadData = (url, page = 1) => {
             const personajes = respJson.results;
             console.log(info.next);
             console.log(info.prev);
-            creaButtons();
+            //creaButtons();
             if (!info.prev) {
                 document.querySelector('#prev').classList.add('disabled')
             } else {
-            document.querySelector('#prev').classList.remove('disabled')
-                //document.querySelector('#prev').setAttribute('data-id', page - 1)
+                document.querySelector('#prev').classList.remove('disabled')
+                document.querySelector('#prev').setAttribute('data-id', Number(page) - 1)
             }
             if (!info.next) {
                 document.querySelector('#next').classList.add('disabled')
             } else {
                 document.querySelector('#next').classList.remove('disabled')
-                //document.querySelector('#next').setAttribute('data-id', page + 1)
+                document.querySelector('#next').setAttribute('data-id', Number(page) + 1)
             }
             console.log(personajes);
             showCharacters(personajes);
         })
 }
+
+const loadCharacterInfo = (url, id) => {
+    let urlCharacter = `${url}${id}`;
+    console.log(urlCharacter);
+    fetch(urlCharacter)
+        .then(respuesta => respuesta.json())
+        .then(personaje => {
+            //TODO: Implementar Modal con info del personaje
+            console.log(personaje);
+            alert(personaje.name);
+        });
+}
+
+const showModal = (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains('btn')) {
+        let id = e.target.getAttribute('data-id');
+        loadCharacterInfo(urlBase, id);
+    }
+}
+
+document.querySelector('#respuesta').addEventListener('click', showModal);
 
 const navegacion = (e) => {
     e.preventDefault();
@@ -36,12 +58,13 @@ const navegacion = (e) => {
 }
 
 loadData(urlBase);
-//document.querySelector('.botones').addEventListener('click', navegacion);
+
+document.querySelector('.botones').addEventListener('click', navegacion);
 
 const showCharacters = (personajes) => {
     const contenedorRespuesta = document.querySelector('#respuesta');
     while (contenedorRespuesta.firstChild) {
-        contenedorRespuesta.remove(contenedorRespuesta.firstChild);
+        contenedorRespuesta.removeChild(contenedorRespuesta.firstChild);
     }
     personajes.forEach(personaje => {
         contenedorRespuesta.appendChild(creaCard(personaje));
@@ -53,31 +76,29 @@ const creaCard = (personaje) => {
     card.style = 'float: left;';
     const html = `
     <div class="card m-2" style="width: 18rem; ">
-        <img src="${personaje.image}" class="card-img-top" alt="...">
+        <img loading="lazy" src="${personaje.image}" class="card-img-top" alt="...">
         <div class="card-body">
         <h5 class="card-title">${personaje.name}</h5>
         <p class="card-text">${personaje.status}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <button class="btn btn-primary" data-id="${personaje.id}">Ver más</a>
         </div>
     </div>`;
     card.innerHTML = html;
     return card;
 }
 
-// <button id="prev" class="btn btn-success btn-l" data-id="">Anterior</button>
-//<button id="next" class="btn btn-success btn-l" data-id="">Siguiente</button>
 
-const creaButtons = () => {
-    const contenedorButtons = document.querySelector('#botones');
-    contenedorButtons.innerText = '';
-    const btnPrev = document.createElement('button');
-    btnPrev.id = 'prev';
-    btnPrev.className = 'btn btn-success btn-lg mx-3';
-    btnPrev.innerText = 'Anterior';
-    contenedorButtons.appendChild(btnPrev);
-    const btnNext = document.createElement('button');
-    btnNext.id = 'next';
-    btnNext.className = 'btn btn-success btn-lg mx-3';
-    btnNext.innerText = 'Siguiente';
-    contenedorButtons.appendChild(btnNext);
-}
+//const creaButtons = () => {
+    //const contenedorButtons = document.querySelector('#botones');
+    //contenedorButtons.innerText = '';
+    //const btnPrev = document.createElement('button');
+   // btnPrev.id = 'prev';
+    //btnPrev.className = 'btn btn-success btn-lg mx-3';
+   // btnPrev.innerText = 'Anterior';
+    //contenedorButtons.appendChild(btnPrev);
+    //const btnNext = document.createElement('button');
+    //btnNext.id = 'next';
+    //btnNext.className = 'btn btn-success btn-lg mx-3';
+    //btnNext.innerText = 'Siguiente';
+    //contenedorButtons.appendChild(btnNext);
+//}
